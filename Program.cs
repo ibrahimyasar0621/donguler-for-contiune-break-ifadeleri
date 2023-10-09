1fe14a878082e1a12e2a2b0e5b1398dccf6eb8d2
ibrahimyasar2106 <ibrahimyasar724@gmail.com>
2023-10-09 19:18:02 +0300

    patika-donguler-for-break-continue-ifadeleri

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace donguler_for_loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ekrana girilen sayılara kadar olan tek sayıalrı yazdır
+        // Console.WriteLine("Lütfen Bir Sayı Giriniz:");
+        // int sayac = int.Parse(Console.ReadLine());   //  sadece consolereadline yazarsam hata verir çünkü consolereadline string okuması yapar. bu sebeple integer'i parse'a kest ediyırum. PARSE string ifadeleri çevirmede kullanılır.
+        // for (int i = 1; i <= sayac; i++)
+        // {
+        //     if(i%2 == 1);
+        //     Console.WriteLine(i);
+
+
+        // }
+
+        // // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdır
+        // int tekToplam = 0;
+        // int ciftToplam = 0;
+        // for (int i =1; i < 1000; i++)
+        // {
+        //     if(i%2 == 1)         
+        //      tekToplam += i;             // tektoplam = tektoplam + i 
+        //     else
+        //      ciftToplam += i;           // cifttoplam = cifttoplam + i
+            
+
+
+        // }
+        // Console.WriteLine("Tek Toplam:" + tekToplam);
+        // Console.WriteLine("Cift Toplam:" + ciftToplam);
+
+        //BREAK VE CONTİUNE
+        //BREAK =DÖNGÜYÜ SONLANDIRMAK İSTİYORSANIZ BREAK İFADESİNİ KULLANIRSINIZ
+        //CONTİUNE=MEVCUT DÖNGÜDE ATLAMAK İSTEDİĞİNİZ ŞARTI YAZIYORUZ VE DÖNGÜ ORAYI ATLAYIP DEVAM EDİYOR.
+        
+        for (int i = 1; i < 10; i++)
+        {
+            if(i==4)
+             break;
+            Console.WriteLine(i);  //SONUÇ=1  2  3  YAZACAK ÇÜNKÜ  4 E EŞİT OLUNCA DURDUR EDİK
+        }
+        for (int i = 1; i < 10; i++)
+        {
+            if(i==4)
+             continue;
+            Console.WriteLine(i);  //SONUÇ=1  2  3  YAZACAK ÇÜNKÜ  4 E EŞİT OLUNCA DURDUR EDİK
+        }
+
+
+
+    }
+}
