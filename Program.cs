7a1a7c22018dc48d63b01ba117b25bec624447a2
ibrahimyasar2106 <ibrahimyasar724@gmail.com>
2023-10-09 19:51:44 +0300

    hackerrank-odevi--eklendi

--- Program.cs
+++ Program.cs
@@ -49,6 +49,25 @@ class Program
         }
 
 
+        //HACKERRANK ÖDEVİ
+//         using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("");
+//         int n = int.Parse(Console.ReadLine());
+
+//         for (int i = 1; i <= 10; i++)
+//         {
+//             int result = n * i;
+//             Console.WriteLine($"{n} x {i} = {result}");
+//         }
+    }
+}
+
+
 
     }
 }
